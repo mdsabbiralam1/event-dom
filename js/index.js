eebf4d3a9c90d0c494c8914ebec882f860ec4282
MdSabbir Alam <mdsabbiralam009@gmail.com>
2022-01-23 12:35:32 +0600

    added three approach in event handler

--- js/index.js
+++ js/index.js
@@ -6,3 +6,11 @@ function orangeColor () {
                document.body.style.color = "white"
 }
 
+// handle green button click by setting function namemake-green-button
+function greenColor () {
+               document.body.style.backgroundColor = "green";
+}
+
+const greenClr = document.getElementById('make-green-button');
+// just set the name function name
+greenClr.onclick = greenColor();
