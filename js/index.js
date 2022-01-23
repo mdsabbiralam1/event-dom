f9568d4e61b1dbaec34ffd412fd4a91549a1f604
MdSabbir Alam <mdsabbiralam009@gmail.com>
2022-01-23 12:21:54 +0600

    added event handler

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function redColor () {
+               document.body.style.backgroundColor = "red";
+}
+function orangeColor () {
+               document.body.style.backgroundColor = "orange";
+               document.body.style.color = "white"
+}
+
