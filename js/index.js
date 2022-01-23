f809cb241a03417716b8e8c1a889dd4ca533a91c
MdSabbir Alam <mdsabbiralam009@gmail.com>
2022-01-23 14:26:12 +0600

    added 7 event listener

--- js/index.js
+++ js/index.js
@@ -14,3 +14,30 @@ function greenColor () {
 const greenClr = document.getElementById('make-green-button');
 // just set the name function name
 greenClr.onclick = greenColor();
+
+
+// four approach-- handle evnet using anonymous function
+const aquaColor = document.getElementById('make-aqua-button');
+aquaColor.onclick = function () {
+               document.body.style.backgroundColor = "aqua";
+}
+// five approach
+const lightblue = document.getElementById('make-lightBlue');
+lightblue.addEventListener('click', lightBlue());
+
+function lightBlue () {
+               document.body.style.backgroundColor = "lightBlue"
+}
+
+
+
+// six approach:--add event listener with function
+const goldenrod = document.getElementById('make-goldenrod');
+goldenrod.addEventListener('click', function() {
+               document.body.style.backgroundColor = "goldenrod"
+})
+
+
+document.getElementById('make-red-btn').addEventListener('click', function(){
+               document.body.style.backgroundColor = "red";
+})
