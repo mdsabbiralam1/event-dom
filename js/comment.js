3ddaee8a89329e53f224ca2cc4f117a13a6d7406
MdSabbir Alam <mdsabbiralam009@gmail.com>
2022-01-24 14:11:14 +0600

    added comment part

---
+++ js/comment.js
@@ -0,0 +1,4 @@
+document.getElementById('comment-submit').addEventListener('click', function () {
+               const newText = document.getElementById('new-text');
+               // console.log(newText.value)
+               
